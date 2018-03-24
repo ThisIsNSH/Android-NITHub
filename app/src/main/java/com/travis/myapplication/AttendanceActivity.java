87ce55916b8a0b5d47679c39b714755ae9199f8f
ThisIsNSH <hadanis.singh@gmail.com>
2018-03-25 01:59:56 +0530

    activity_attendance

---
+++ app/src/main/java/com/travis/myapplication/AttendanceActivity.java
@@ -0,0 +1,13 @@
+package com.travis.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AttendanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance);
+    }
+}
