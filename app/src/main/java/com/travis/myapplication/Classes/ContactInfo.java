4e1276a1731c6d74a8488834ecacf3c3c9d86bae
Abhinavlamba <abhinavlamba826@gmail.com>
2018-03-25 04:00:40 +0530

    announcement added

---
+++ app/src/main/java/com/travis/myapplication/Classes/ContactInfo.java
@@ -0,0 +1,12 @@
+package com.travis.myapplication.Classes;
+
+/**
+ * Created by Reggie on 18-Mar-17.
+ */
+
+public class ContactInfo {
+    public String surName;
+    public String firstName;
+    public String feed;
+    public String feedDate;
+}
