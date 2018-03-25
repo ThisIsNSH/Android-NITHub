774b0eaab7d0d1eb66c857cf28d55b046be7cf6b
Abhinavlamba <abhinavlamba826@gmail.com>
2018-03-25 11:35:20 +0530

    feat:chat

---
+++ app/src/main/java/com/travis/myapplication/Chat.java
@@ -0,0 +1,112 @@
+package com.chatapp.android.androidchatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.HashMap;
+import java.util.Map;
+public class Chat extends AppCompatActivity {
+    LinearLayout layout;
+    RelativeLayout layout_2;
+    ImageView sendButton;
+    EditText messageArea;
+    ScrollView scrollView;
+    Firebase reference1, reference2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        layout = (LinearLayout) findViewById(R.id.layout1);
+        layout_2 = (RelativeLayout)findViewById(R.id.layout2);
+        sendButton = (ImageView)findViewById(R.id.sendButton);
+        messageArea = (EditText)findViewById(R.id.messageArea);
+        scrollView = (ScrollView)findViewById(R.id.scrollView);
+        Firebase.setAndroidContext(this);
+        reference1 = new Firebase("https://androidchatapp-76776.firebaseio.com/messages/" + com.chatapp.android.androidchatapp.UserDetails.username + "_" + com.chatapp.android.androidchatapp.UserDetails.chatWith);
+        reference2 = new Firebase("https://androidchatapp-76776.firebaseio.com/messages/" + com.chatapp.android.androidchatapp.UserDetails.chatWith + "_" + com.chatapp.android.androidchatapp.UserDetails.username);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = messageArea.getText().toString();
+                if(!messageText.equals("")){
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("message", messageText);
+                    map.put("user", com.chatapp.android.androidchatapp.UserDetails.username);
+                    reference1.push().setValue(map);
+                    reference2.push().setValue(map);
+                    messageArea.setText("");
+                }
+            }
+        });
+
+        reference1.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map map = dataSnapshot.getValue(Map.class);
+                String message = map.get("message").toString();
+                String userName = map.get("user").toString();
+
+                if(userName.equals(com.chatapp.android.androidchatapp.UserDetails.username)){
+                    addMessageBox("You:\n" + message, 1);
+                }
+                else{
+                    addMessageBox(com.chatapp.android.androidchatapp.UserDetails.chatWith + ":\n" + message, 2);
+                }
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+    public void addMessageBox(String message, int type){
+        TextView textView = new TextView(Chat.this);
+        textView.setText(message);
+        textView.setPadding(4,4,4,4);
+        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp2.weight = 1.0f;
+        lp2.setMargins(16,16,16,16);
+        if(type == 1) {
+            lp2.gravity = Gravity.RIGHT;
+            textView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        }
+        else{
+            lp2.gravity = Gravity.LEFT;
+            textView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+
+        }
+        textView.setLayoutParams(lp2);
+        layout.addView(textView);
+        scrollView.fullScroll(View.FOCUS_DOWN);
+    }
+}
