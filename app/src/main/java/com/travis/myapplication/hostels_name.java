0b4acb96ef29b35e7bc2ccfbb74860bfab5a7f4a
ThisIsNSH <hadanis.singh@gmail.com>
2018-03-25 10:51:44 +0530

    hostel.java

--- app/src/main/java/com/travis/myapplication/hostels_name.java
+++ app/src/main/java/com/travis/myapplication/hostels_name.java
@@ -1,17 +1,24 @@
 package com.travis.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-/**
- * Created by naman on 25-03-2018.
- */
 public class hostels_name extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hostels_name);
+        RelativeLayout a = findViewById(R.id.kailash);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(hostels_name.this,blocks.class));
+            }
+        });
     }
 }
 
